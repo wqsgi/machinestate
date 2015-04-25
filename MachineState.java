17d0f86ff0fc5e3547ceff68963c609e94bd98c6
wqsgi <wqs_2005@126.com>
2015-04-25 23:14:13 +0800

    wei
    state

---
+++ MachineState.java
@@ -0,0 +1,47 @@
+package com.jd.state;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
+/**
+ * Created by weiqisong on 2015/4/25.
+ */
+public class MachineState {
+
+
+    // 过期时间
+    private int expire;
+
+    private Map<String,Map<Integer,Transaction>> state = new HashMap<>();
+
+    private ConcurrentMap<Long,String> preStates = new ConcurrentHashMap<>();
+
+
+
+
+}
+
+class Transaction {
+
+    private String preType;
+    private String currentType;
+    private boolean isEnd;
+
+    public String getPreType() {
+        return preType;
+    }
+
+    public void setPreType(String preType) {
+        this.preType = preType;
+    }
+
+    public String getCurrentType() {
+        return currentType;
+    }
+
+    public void setCurrentType(String currentType) {
+        this.currentType = currentType;
+    }
+}
